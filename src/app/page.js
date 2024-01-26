3bd88c99adeca93450d3b5e39f44e0738a40dfaf
Martin Dreyer <martinchristiandreyer@gmail.com>
2024-01-26 10:16:11 +0100

    updated input

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,92 @@
-'use client'
-import MapComponent from "./components/MapComponent"
-import { generateHexagons, calculateHexagonCenters, getGeoJSONData } from "./utils/hexagonUtils"
+"use client";
+import { useState } from "react";
+import MapComponent from "./components/MapComponent";
+import {
+  generateHexagons,
+  calculateHexagonCenters,
+} from "./utils/hexagonUtils";
+import fyn from "../../public/data/fyn_";
 
 const bounds = [10.18, 55.3];
-const hexBounds = [9.65, 54.523, 11.282, 56.054]
+const hexBounds = [9.65, 54.69, 11.082, 55.654];
 
-const cellSize = 1; // Adjust as needed
-const setZoom = 8.3
-
+const setZoom = 8.3;
+const mask = fyn;
 
 const IndexPage = () => {
-  const hexagons = generateHexagons(hexBounds, cellSize, );
-  const hexagonCenters = calculateHexagonCenters(hexagons);
-  console.log(hexagonCenters.length)
+  const [cellSize, setCellSize] = useState(10);
+  const [hexagons, setHexagons] = useState([]);
+  const [hexagonCenters, setHexagonCenters] = useState([]);
+
+  const handleInputChange = (e) => {
+    setCellSize(e.target.value);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    // Generate hexagons and calculate centers when the user submits the form
+    const newHexagons = generateHexagons(hexBounds, cellSize, mask);
+    setHexagons(newHexagons);
+
+    const newHexagonCenters = calculateHexagonCenters(newHexagons);
+    setHexagonCenters(newHexagonCenters);
+  };
+
+  const downloadHexCenters = () => {
+    const hexCentersJson = JSON.stringify(hexagonCenters, null, 2);
+    const blob = new Blob([hexCentersJson], { type: "application/json" });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "hexagonCenters.json";
+    a.click();
+    URL.revokeObjectURL(url);
+  };
 
   return (
-    <div className="map-container">
-      <MapComponent center={bounds} zoom={setZoom} hexagonCenters={hexagonCenters} hexGrid={hexagons}>
-      </MapComponent>
-    </div>
+    <>
+      <div className="flex-col flex justify-center items-center w-screen">
+        <div className="p-4 text-center max-w-xs">
+          <p className="p-2">Vælg cellestørrelse</p>
+          <form onSubmit={handleFormSubmit}>
+            <input
+              type="number"
+              className="shadow"
+              value={cellSize}
+              onChange={handleInputChange}
+            ></input>
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-4"
+            >
+              Bekræft
+            </button>
+          </form>
+        </div>
+      </div>
+      {hexagonCenters.length > 0 && (
+        <div className="flex justify-center">
+          <button
+            className="bg-blue-500 m-10 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded z-10"
+            onClick={downloadHexCenters}
+          >
+            Download Hex Centers
+          </button>
+        </div>
+      )}
+      {hexagonCenters.length > 0 && (
+        <div className="map-container flex justify-center">
+          <MapComponent
+            center={bounds}
+            zoom={setZoom}
+            hexagonCenters={hexagonCenters}
+            hexGrid={hexagons}
+          ></MapComponent>
+        </div>
+      )}
+    </>
   );
 };
 
-export default IndexPage
+export default IndexPage;
